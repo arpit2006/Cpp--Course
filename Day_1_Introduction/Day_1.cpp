227cdc1398eaf7897770988ec9ac3ecb40d306e9
arpit2006 <arpitshirbhate4@gmail.com>
2025-01-22 23:16:24 +0530

    initial commit

--- Day_1_Introduction/Day_1.cpp
+++ Day_1_Introduction/Day_1.cpp
@@ -1,7 +1,8 @@
-#include<iostream> // header file
-using namespace std; // namespace : std is the standard library //std : 
-int main()
+#include<iostream> //This is a preprocessor directive that includes the iostream (input-output stream) library.
+using namespace std;//This tells the compiler to use the standard namespace, so you don't need to prefix standard library objects with std::.
+int main()//his is the main function where the program starts execution. Every C++ program must have a main function
 {
-    cout << "Hello World...! ";
-    return 0;
+    cout << "Hello World..!"; //console outpu
+
+    return 0; //Returning 0 from main signals to the operating system that the program completed without errors.
 }
